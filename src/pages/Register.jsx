513a883f760a2138d71fac62670f7d0d09f9c767
Mahi Chaudhary <mahic8317@gmail.com>
2026-06-17 16:52:17 +0530

    test register update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,62 +1,91 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Register = () => {
+function Register() {
+  const navigate = useNavigate();
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-     console.log("Register button clicked");
 
-    const res = await fetch(
-  "https://shophub-ecommerce-7gbt.onrender.com/api/auth/register",
-  {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name,
-      email,
-      password,
-    }),
-  }
-);
-    const data = await res.json();
-    alert(data.message);
+    console.log("Register button clicked");
+
+    try {
+      const res = await fetch(
+        "https://shophub-ecommerce-7gbt.onrender.com/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+          }),
+        }
+      );
+
+      const data = await res.json();
+
+      alert(data.message);
+
+      if (res.ok) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error("Register Error:", error);
+      alert("Registration Failed");
+    }
   };
 
   return (
     <div style={{ padding: "20px" }}>
-      <h2>Register</h2>
+      <h2>Register Page Updated</h2>
 
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Name"
+          value={name}
           onChange={(e) => setName(e.target.value)}
+          required
         />
-        <br /><br />
+
+        <br />
+        <br />
 
         <input
           type="email"
           placeholder="Email"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
-        <br /><br />
+
+        <br />
+        <br />
 
         <input
           type="password"
           placeholder="Password"
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
-        <br /><br />
 
-        <button type="submit">Register</button>
+        <br />
+        <br />
+
+        <button type="submit">
+          Register
+        </button>
       </form>
     </div>
   );
-};
+}
 
 export default Register;
