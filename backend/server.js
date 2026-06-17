f49f7186c995710eb3dbf45a6552cb4f080da211
Mahi Chaudhary <mahic8317@gmail.com>
2026-06-17 17:39:12 +0530

    add root route

--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("ShopHub Backend API is Running 🚀");
+});
